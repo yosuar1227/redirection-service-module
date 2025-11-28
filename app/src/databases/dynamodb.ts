d4ed41c4316a1e815ee99b0baae2eb8e85528ea1
yosuar1227 <yosuar.mieles@unicolombo.edu.co>
2025-11-27 19:24:08 -0500

    feat: add lambda logic and dynamo db service to get the item from the data base

--- app/src/databases/dynamodb.ts
+++ app/src/databases/dynamodb.ts
@@ -1,1 +1,48 @@
-//dynamodb configguration
+//dynamodb configgurationimport { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import {
+    DynamoDBDocumentClient,
+    GetCommand,
+} from "@aws-sdk/lib-dynamodb";
+
+export const LINK_CODE_GROUP = 'LINK_CODE';
+
+export class DynamoService {
+
+    private readonly client = DynamoDBDocumentClient.from(
+        new DynamoDBClient({}),
+        {
+            marshallOptions: {
+                convertClassInstanceToMap: true,
+                removeUndefinedValues: true,
+            },
+        }
+    );
+
+    constructor() { }
+
+    async get(urlCode: string) {
+        if (!urlCode.startsWith(LINK_CODE_GROUP)) {
+            urlCode = `${LINK_CODE_GROUP}-${urlCode}`
+        }
+
+        try {
+            const comand = new GetCommand({
+                TableName: process.env.ShortLinkTable || "",
+                Key: {
+                    uuid: urlCode,
+                    linkCode: LINK_CODE_GROUP
+                },
+            });
+
+            const response = await this.client.send(comand);
+
+            return response.Item || null;
+        } catch (error) {
+            console.error("Error getting the item", error);
+            return null;
+        }
+    }
+
+
+}
--- app/src/handlers/redirect-url-lambda.ts
+++ app/src/handlers/redirect-url-lambda.ts
@@ -1,1 +1,36 @@
 //lambda to redirect by code of the path url if the code exits in the data base
+import { APIGatewayEvent, APIGatewayProxyResult } from "aws-lambda";
+import createHttpError from "http-errors";
+import { DynamoService } from "../databases/dynamodb";
+import middy from "@middy/core";
+import httpErrorHandler from "@middy/http-error-handler";
+
+const redirectToUrlByCodeLambda = async (
+    event: APIGatewayEvent
+): Promise<APIGatewayProxyResult> => {
+    const urlCode = event.pathParameters?.urlCode;
+
+    if (!urlCode) {
+        throw createHttpError.BadRequest("Missing mandatory urlCode value.");
+    }
+
+    const urlDataFromDb = await new DynamoService().get(urlCode);
+
+    if (urlDataFromDb === null) {
+        throw createHttpError.BadRequest(`Url code ${urlCode} does not exits in the data base.`)
+    }
+
+    console.log("URL DATA FORM DATA BASE::", urlDataFromDb);
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(urlDataFromDb),
+        headers: {
+            "Content-type": "application/json",
+        }
+    }
+}
+
+export const handler = middy<APIGatewayEvent, APIGatewayProxyResult>(
+    redirectToUrlByCodeLambda
+).use(httpErrorHandler())
